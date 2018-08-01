dd1383ae81536b75cd27236b63972ff6b2853a30
Marcus Hiles <39972880+marcus-hiles@users.noreply.github.com>
2018-08-01 00:15:46 -0500

    Update blocks.js

--- gutenberg/src/blocks.js
+++ gutenberg/src/blocks.js
@@ -1,13 +1,5 @@
 /**
- * Gutenberg Blocks
- *
- * All blocks related JavaScript.
- * You can create a new block folder in
- * this dir and include code for that block
- * here as well.
- *
- * All blocks should be included here since
- * this is the file that Webpack is compiling.
+ * Gutenberg Block
  */
 
 import './block/block.js';
